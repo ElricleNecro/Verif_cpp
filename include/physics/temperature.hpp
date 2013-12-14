30c42b46a9290ee7266ecdc681c46d0b07db005d
ElricleNecro <guiplum@gmail.com>
2013-12-14 13:00:50 +0100

    Add method to temperature class to get class name. Add a new class to calculate temperature from density.

--- include/physics/temperature.hpp
+++ include/physics/temperature.hpp
@@ -16,6 +16,8 @@ namespace Stats {
 			Temperature(io::types::Particules part, const int NbPart, const double r_min, const double r_max, const int bin=300, const double r_norm=1.0);
 			virtual ~Temperature(void);
 
+			const char* GetName(void);
+
 			void Calcul(void);
 			void Calcul(const Density &densite);
 
@@ -24,6 +26,23 @@ namespace Stats {
 
 		private:
 			double t_moy = 0.;
+	};
+
+	class DensityTemperature : public Histogram {
+		public:
+			DensityTemperature(io::types::Particules part, const int NbPart, const double r_min, const double r_max, const int bin=300, const double r_norm=1.0);
+			virtual ~DensityTemperature(void);
+
+			const char* GetName(void);
+
+			void Calcul(void);
+
+			using Histogram::operator();
+			using Histogram::operator[];
+
+		private:
+			double t_moy = 0.;
+			Density densite;
 	};
 }
 
--- src/physics/temperature.cpp
+++ src/physics/temperature.cpp
@@ -16,6 +16,11 @@ namespace Stats {
 	{
 	}
 
+	const char* Temperature::GetName(void)
+	{
+		return "Temperature";
+	}
+
 	void Temperature::Calcul(void)
 	{
 		double  r        = 0.;
@@ -57,6 +62,71 @@ namespace Stats {
 		double d_all       = 0.0,
 		       r           = 0.0;
 		int   *compteur    = NULL;
+
+		compteur    = new int[this->NbBin];
+		for(int j=0; j<this->NbBin; j++)
+		{
+			compteur[j]   = 0;
+			this->bins[j] = 0.;
+		}
+
+		for(int i = 0; i < this->NbPart; i++)
+		{
+			for(int j=0; j<this->NbBin; j++)
+			{
+				r = Utils::rayon(&part[i]);
+				if( r > this->r[j] && r <= this->r[j+1] )
+				{
+					compteur[j]++;
+					this->bins[j] += Utils::v2(&this->part[i]);
+					break;
+				}
+			}
+		}
+
+		for(int i = 0; i < this->NbBin; i++)
+		{
+			this->bins[i] *= densite[i] / ( (double)( (compteur[i] != 0)?compteur[i]:1.0 ) );
+			d_all         += densite[i];
+		}
+
+		this->t_moy = 0.0;
+		for (int i = 0; i < this->NbBin; i++) {
+			this->t_moy   += this->bins[i];
+			this->bins[i] /= d_all;
+		}
+		this->t_moy /= d_all;
+
+		delete[] compteur;
+	}
+
+	DensityTemperature::DensityTemperature(io::types::Particules part,
+			const int NbPart,
+			const double r_min,
+			const double r_max,
+			const int bin,
+			const double r_norm
+			) : Histogram(part, NbPart, r_min, r_max, bin, r_norm),
+			    densite(part, NbPart, r_min, r_max, bin, r_norm)
+	{
+		this->t_moy = 0.;
+	}
+
+	DensityTemperature::~DensityTemperature(void)
+	{
+	}
+
+	const char* DensityTemperature::GetName(void)
+	{
+		return "DensityTemperature";
+	}
+
+	void DensityTemperature::Calcul(void)
+	{
+		double d_all       = 0.0,
+		       r           = 0.0;
+		int   *compteur    = NULL;
+		this->densite.Calcul();
 
 		compteur    = new int[this->NbBin];
 		for(int j=0; j<this->NbBin; j++)
